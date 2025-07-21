cc8682fe37819d48c931b9d23f05b131e0e1472f
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-07-21 00:10:59 -0600

    Create Book Model

--- db.ts
+++ db.ts
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const mysql2 = require("mysql2");
 require("dotenv").config();
 
@@ -12,9 +12,40 @@ const sequelize = new Sequelize({
   database: process.env.DB_NAME,
 });
 
+const Book = sequelize.define("Book", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  bookAuthor: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  year: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  isbn: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  dateRead: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+});
+
 try {
   sequelize.authenticate();
   console.log("Connection has been established successfully.");
+  sequelize.sync({ force: true }).then(() => {
+    console.log("Database & tables created!");
+  });
 } catch (error) {
   console.error("Unable to connect to the database:", error);
 }
